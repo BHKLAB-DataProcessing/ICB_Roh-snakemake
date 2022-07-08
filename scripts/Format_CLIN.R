8d25b45bd59f8c00249e8169a6cd25215e3a6d1b
Minoru Nakano <minoru.nakano@gmail.com>
2022-07-08 17:29:50 -0400

    update clin processing

--- scripts/Format_CLIN.R
+++ scripts/Format_CLIN.R
@@ -3,23 +3,29 @@ input_dir <- args[1]
 output_dir <- args[2]
 
 source("https://raw.githubusercontent.com/BHKLAB-Pachyderm/ICB_Common/main/code/Get_Response.R")
+source("https://raw.githubusercontent.com/BHKLAB-Pachyderm/ICB_Common/main/code/format_clin_data.R")
 
-clin = read.csv( file.path(input_dir, "CLIN.txt"), stringsAsFactors=FALSE , sep="\t")
-
-clin = cbind( clin[ , c( "Patient.ID","Sex","Age","anti.CTLA.4.RECIST..CR..PR..SD..PD.","Overall.Survival","Dead..Y.N." ) ] ,
-	 "Melanoma" , "CTLA4" , NA , NA , NA , NA , NA , NA , NA , NA , NA )
+clin_original = read.csv( file.path(input_dir, "CLIN.txt"), stringsAsFactors=FALSE , sep="\t")
+selected_cols <- c( "Patient.ID","Sex","Age","anti.CTLA.4.RECIST..CR..PR..SD..PD.","Overall.Survival","Dead..Y.N." )
+clin = cbind( clin_original[ , selected_cols ] ,"Melanoma" , "CTLA4" , NA , NA , NA , NA , NA , NA , NA , NA , NA )
 colnames(clin) = c( "patient" , "sex" , "age" , "recist" , "t.os" , "os"  , 
 					"primary" , "drug_type" , "histo" , "stage" , "t.pfs" , "pfs" , "dna" , "rna" , "response.other.info" , "response")
 
 clin$patient = paste( "P." , clin$patient , sep="" )
+clin_original$Patient.ID <- paste0("P.", clin_original$Patient.ID)
+
 clin$t.os = clin$t.os / 30.5
 clin$os = ifelse(clin$os %in% "Y" , 1 , 0 )
 clin$response = Get_Response( data=clin )
 
 case = read.csv( file.path(output_dir, "cased_sequenced.csv"), stringsAsFactors=FALSE , sep=";" )
-clin = clin[ clin$patient %in% case$patient , ]
-clin$dna[ clin$patient %in% case[ case$snv %in% 1 , ]$patient ] = "wes"
 
+clin = clin[ clin$patient %in% case$patient , ]
+clin_original <- clin_original[clin_original$Patient.ID %in% case$patient, ]
+
+clin$dna[ clin$patient %in% case[ case$snv %in% 1 , ]$patient ] = "wes"
 clin = clin[ , c("patient" , "sex" , "age" , "primary" , "histo" , "stage" , "response.other.info" , "recist" , "response" , "drug_type" , "dna" , "rna" , "t.pfs" , "pfs" , "t.os" , "os" ) ]
+
+clin <- format_clin_data(clin_original, 'Patient.ID', selected_cols, clin)
 
 write.table( clin , file=file.path(output_dir, "CLIN.csv") , quote=FALSE , sep=";" , col.names=TRUE , row.names=FALSE )
