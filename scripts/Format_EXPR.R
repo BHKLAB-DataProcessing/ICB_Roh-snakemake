174e7b8c70e45a8e58214be386f57d878d3e3f5e
Minoru Nakano <minoru.nakano@gmail.com>
2022-08-05 14:30:23 -0400

    added log transformation

--- scripts/Format_EXPR.R
+++ scripts/Format_EXPR.R
@@ -15,4 +15,7 @@ expr = data.frame(expr[ , grep("A" , colnames(expr) ) ])
 colnames(expr) <- str_replace(colnames(expr), 'X', '')
 colnames(expr) = sapply( colnames(expr) , function(x){ paste( "P.", unlist(strsplit( x , "A" , fixed=TRUE))[1] , sep="" ) } )
 
-write.table( expr , file=file.path(output_dir, "EXPR.csv"), quote=FALSE , sep=";" , col.names=TRUE , row.names=TRUE )
+tpm <- (2 ^ expr) - 1
+tpm <- log2(tpm + 0.001)
+
+write.table( tpm , file=file.path(output_dir, "EXPR.csv"), quote=FALSE , sep=";" , col.names=TRUE , row.names=TRUE )
